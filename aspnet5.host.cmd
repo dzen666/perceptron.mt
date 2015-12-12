f1d0156e2b4639fdab3237aea568285867a6eabf
valery sntx <valery.sntx@gmail.com>
2015-12-12 18:42:18 +0200

    removing junk files

--- aspnet5.host.cmd
+++
@@ -1,2 +0,0 @@
- @powershell -NoProfile -ExecutionPolicy unrestricted -Command "&{$Branch='dev';$wc=New-Object System.Net.WebClient;$wc.Proxy=[System.Net.WebRequest]::DefaultWebProxy;$wc.Proxy.Credentials=[System.Net.CredentialCache]::DefaultNetworkCredentials;Invoke-Expression ($wc.DownloadString('https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.ps1'))}"
- 
