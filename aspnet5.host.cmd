b07758d4b524a0ae957b840e8527f6f978931799
valery.sntx <valery.sntx@gmail.com>
2015-12-12 06:06:33 +0200

    removed junk

--- aspnet5.host.cmd
+++ aspnet5.host.cmd
@@ -1,3 +1,2 @@
  @powershell -NoProfile -ExecutionPolicy unrestricted -Command "&{$Branch='dev';$wc=New-Object System.Net.WebClient;$wc.Proxy=[System.Net.WebRequest]::DefaultWebProxy;$wc.Proxy.Credentials=[System.Net.CredentialCache]::DefaultNetworkCredentials;Invoke-Expression ($wc.DownloadString('https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.ps1'))}"
  
- npm install novabrain --save
